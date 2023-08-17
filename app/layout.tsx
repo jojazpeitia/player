97d6760d6e63076f06b6fcc3b03b1766ba372a83
jojazpeitia <jojazpeitia@gmail.com>
2023-08-17 16:55:18 -0500

    fixed device viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'ee oh',
   description: 'ohhhh',
+  viewport: 'width=512, initial-scale=0.8'
 }
 
 export default function RootLayout({
